89ba0ec20456cdf2eff84e07252414d754f0b6ed
Jordan Wong <pjwong97@gmail.com>
2022-08-09 12:33:54 -0700

    complete User model

--- models.py
+++ models.py
@@ -5,15 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-class MyTable(db.Model):
-    """Playlist."""
-
-    __tablename__ = "mytables"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-
 def connect_db(app):
     """Connect this database to provided Flask app.
 
@@ -22,3 +13,24 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+    """Playlist."""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20),
+                nullable = False,
+                primary_key = True)
+    password = db.Column(db.String(100),
+                nullable = False)
+    email = db.Column(db.String(50),
+                nullable = False,
+                unique = True)
+    first_name = db.Column(db.String(30),
+                nullable = False)
+    last_name = db.Column(db.String(30),
+                nullable = False)
+
+
+
